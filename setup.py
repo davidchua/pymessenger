1fffcdf2b12b0dd4d21cb77fd8ef9da6ec5e4ccc
David Chua <zhchua@gmail.com>
2016-04-14 14:54:16 +0800

    Bump to 0.0.3.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 setup(
   name = 'pymessenger',
   packages = ['pymessenger'],
-  version = '0.0.3',
+  version = '0.0.3.1',
   install_requires=[
         'requests'
   ],
