7a18753f683b9c84fbbe1fec17a5578bdad0a7c9
David Chua <zhchua@gmail.com>
2016-04-14 11:59:10 +0800

    Bump to 0.0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 setup(
   name = 'pymessenger',
   packages = ['pymessenger'],
-  version = '0.0.1',
+  version = '0.0.2',
 #  install_requires=[
 #        'xmltodict','requests'
 #  ],
@@ -10,7 +10,7 @@ setup(
   author = 'David Chua',
   author_email = 'zhchua@gmail.com',
   url = 'https://github.com/davidchua/pymessenger',
-  download_url = 'https://github.com/davidchua/pymessenger/tarball/0.0.1',
+  download_url = 'https://github.com/davidchua/pymessenger/tarball/0.0.2',
   keywords = ['facebook messenger', 'python', 'wrapper', 'bot', 'messenger bot'],
   classifiers = [],
 )
