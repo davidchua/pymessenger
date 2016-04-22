8b42423ca0ddc6ad99094336f1834ec89d2c2b51
daniel.klein <daniel.klein@oracle.com>
2016-04-22 10:38:55 -0400

    Messages were being preformatted; fixed

--- pymessenger/bot.py
+++ pymessenger/bot.py
@@ -7,45 +7,26 @@ class Bot:
         self.base_url = "https://graph.facebook.com/v2.6/me/messages?access_token={0}".format(access_token)
 
     def send_text_message(self, recipient_id, message):
-        recipient = {
-                "id": recipient_id
-                }
-        message = {
-                "text": message
-                }
-        payload ={}
-        payload['recipient'] = "{0}".format(recipient)
-        payload['message'] = "{0}".format(message)
-        return requests.post(self.base_url, data=(payload)).json()
+        payload = {'recipient': {'id': recipient_id},
+                   'message': {'text': message}
+                  }
+        return requests.post(self.base_url, json=payload).json()
 
     def send_message(self, recipient_id, message):
-        recipient = {
-                "id": recipient_id
-                }
-        payload ={}
-        payload['recipient'] = "{0}".format(recipient)
-        payload['message'] = "{0}".format(message)
-        return requests.post(self.base_url, data=(payload)).json()
+        payload ={'id': recipient_id,
+                  'message': message
+                 }
+        return requests.post(self.base_url, json=payload).json()
 
     def send_generic_message(self, recipient_id, elements):
-        recipient = {
-                "id": recipient_id
-                }
-        message = {
-                "attachment": {
-                    "type": "template",
-                    "payload": {
-                        "template_type": "generic",
-                        "elements": elements
-                        }
-                    }
-                }
-        payload = {}
-        payload['recipient'] = "{0}".format(recipient)
-        payload['message'] = "{0}".format(message)
-        json_payload = {
-                'recipient': recipient,
-                'message': message
-                }
-        return requests.post(self.base_url, json=json_payload).json()
-
+        payload = {'recipient': {'id': recipient_id},
+                   'message': { "attachment": {
+                                "type": "template",
+                                "payload": {
+                                    "template_type": "generic",
+                                    "elements": elements
+                                    }
+                                }
+                              }
+                   }
+        return requests.post(self.base_url, json=payload).json()
