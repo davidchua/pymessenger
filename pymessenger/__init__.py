57b61aa65a6722d6803c4543c36909dff1e9ef05
drice <daniel.rice@viasat.com>
2016-04-29 01:32:09 +0000

    PEP8, simplify for readability

--- pymessenger/__init__.py
+++ pymessenger/__init__.py
@@ -1,17 +1,14 @@
 import json
-class Element:
 
-    __acceptable_keys_list = ['title', 'item_url', 'image_url', 'subtitle']
+class Element(object):
+
+    __acceptable_keys = ['title', 'item_url', 'image_url', 'subtitle']
     def __init__(self, **kwargs):
-#        ipdb.set_trace()
-        [self.__setattr__(key, kwargs.get(key)) for key in self.__acceptable_keys_list]
+        for key in self.__acceptable_keys:
+            setattr(self, key, kwargs.get(key))
 
     def to_json(self):
-        data = {
-                "title": self.title,
-                "item_url": self.item_url,
-                "image_url": self.image_url,
-                "subtitle": self.subtitle
-                }
+        data = {}
+        for key in self.__acceptable_keys:
+            data[key] = getattr(self, key)
         return json.dumps(data)
-
