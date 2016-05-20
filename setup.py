bf1cbbf792e8e05f79fbf742ffc5491c9f10986c
David Chua <zhchua@gmail.com>
2016-05-20 16:36:18 +0800

    Bump to 0.0.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 setup(
   name = 'pymessenger',
   packages = ['pymessenger'],
-  version = '0.0.3.1',
+  version = '0.0.4.0',
   install_requires=[
         'requests',
         'requests-toolbelt'
