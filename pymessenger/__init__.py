42698443fc9161cd5513296b5c759d901cc759d7
drice <daniel.rice@viasat.com>
2016-06-17 17:37:12 +0000

    update elements with buttons

--- pymessenger/__init__.py
+++ pymessenger/__init__.py
@@ -3,10 +3,14 @@ import json
 from .bot import Bot
 
 class Element(dict):
-    __acceptable_keys = ['title', 'item_url', 'image_url', 'subtitle']
+    __acceptable_keys = ['title', 'item_url', 'image_url', 'subtitle', 'buttons']
     def __init__(self, *args, **kwargs):
         kwargs = {k:v for k, v in kwargs.iteritems() if k in self.__acceptable_keys}
         super(Element, self).__init__(*args, **kwargs)
 
     def to_json(self):
         return json.dumps({k:v for k, v in self.iteritems() if k in self.__acceptable_keys})
+
+class Button(dict):
+    # TODO: Decide if this should do more
+    pass
