84ecd32aa8aa2615583a5ff420a5316fdff458df
drice <daniel.rice@viasat.com>
2016-06-29 19:20:26 +0000

    add test for elements message

--- test/message_test.py
+++ test/message_test.py
@@ -1,10 +1,13 @@
 import sys, os
 sys.path.append(os.path.realpath(os.path.dirname(__file__)+"/.."))
+
 from pymessenger.bot import Bot as PyBot
 from pymessenger import Element
 
 TOKEN = os.environ.get('TOKEN')
-bot = PyBot(TOKEN)
+APP_SECRET = os.environ.get('APP_SECRET')
+
+bot = PyBot(TOKEN, app_secret=APP_SECRET)
 recipient_id = os.environ.get('RECIPIENT_ID')
 
 def test_wrong_format_message():
@@ -14,6 +17,16 @@ def test_wrong_format_message():
 
 def test_text_message():
     result = bot.send_text_message(recipient_id, "test")
+    assert type(result) is dict
+    assert result.get('message_id') is not None
+    assert result.get('recipient_id') is not None
+
+def test_elements():
+    image_url = 'https://lh4.googleusercontent.com/-dZ2LhrpNpxs/AAAAAAAAAAI/AAAAAAAA1os/qrf-VeTVJrg/s0-c-k-no-ns/photo.jpg'
+    elements = []
+    element = Element(title="Arsenal", image_url=image_url, subtitle="Click to go to Arsenal website.", item_url="http://arsenal.com")
+    elements.append(element)
+    result = bot.send_generic_message(recipient_id, elements)
     assert type(result) is dict
     assert result.get('message_id') is not None
     assert result.get('recipient_id') is not None
