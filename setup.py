84349a16390b053b1b25ae363cc39a25fb9dc13b
Charles-William Crete <git@charles-william.me>
2016-09-10 13:33:41 -0400

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 setup(
   name = 'pymessenger',
   packages = ['pymessenger'],
-  version = '0.0.6.0',
+  version = '0.0.7.0',
   install_requires=[
         'requests',
         'requests-toolbelt',
@@ -12,7 +12,7 @@ setup(
   author = 'David Chua',
   author_email = 'zhchua@gmail.com',
   url = 'https://github.com/davidchua/pymessenger',
-  download_url = 'https://github.com/davidchua/pymessenger/tarball/0.0.6',
+  download_url = 'https://github.com/davidchua/pymessenger/tarball/0.0.7',
   keywords = ['facebook messenger', 'python', 'wrapper', 'bot', 'messenger bot'],
   classifiers = [],
 )
