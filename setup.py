2701b3af8af5716caea830bbe7e249262b2e0e3d
Charles-William Crete <git@charles-william.me>
2016-09-20 18:13:12 -0400

    Fix versioning & bump to 0.7.1

--- setup.py
+++ setup.py
@@ -1,18 +1,19 @@
 from setuptools import setup
+
 setup(
-  name = 'pymessenger',
-  packages = ['pymessenger'],
-  version = '0.0.7.0',
-  install_requires=[
+    name='pymessenger',
+    packages=['pymessenger'],
+    version='0.7.1',
+    install_requires=[
         'requests',
         'requests-toolbelt',
         'six'
-  ],
-  description = "Python Wrapper for FB Messenger Bot",
-  author = 'David Chua',
-  author_email = 'zhchua@gmail.com',
-  url = 'https://github.com/davidchua/pymessenger',
-  download_url = 'https://github.com/davidchua/pymessenger/tarball/0.0.7',
-  keywords = ['facebook messenger', 'python', 'wrapper', 'bot', 'messenger bot'],
-  classifiers = [],
+    ],
+    description="Python Wrapper for FB Messenger Bot",
+    author='David Chua',
+    author_email='zhchua@gmail.com',
+    url='https://github.com/davidchua/pymessenger',
+    download_url='https://github.com/davidchua/pymessenger/tarball/0.7.1',
+    keywords=['facebook messenger', 'python', 'wrapper', 'bot', 'messenger bot'],
+    classifiers=[],
 )
