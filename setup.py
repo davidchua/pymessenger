5ef2001f97891d9301e9937bb087ab819f684c4e
Daniel Rice <drice304@gmail.com>
2016-11-29 13:32:19 -0700

    check if enum installed and add to installation_requirements if not. This is a fix for Python 2 installations

--- setup.py
+++ setup.py
@@ -1,14 +1,22 @@
 from setuptools import setup
 
+installation_requirements = [
+    'requests',
+    'requests-toolbelt',
+    'six'
+]
+
+try:
+    import enum
+    del enum
+except ImportError:
+    installation_requirements.append('enum')
+
 setup(
     name='pymessenger',
     packages=['pymessenger'],
     version='1.0.0',
-    install_requires=[
-        'requests',
-        'requests-toolbelt',
-        'six'
-    ],
+    install_requires=installation_requirements,
     description="Python Wrapper for Facebook Messenger Platform",
     author='David Chua',
     author_email='zhchua@gmail.com',
