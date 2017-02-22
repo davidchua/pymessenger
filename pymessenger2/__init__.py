9c880c6d020bb03bfcf0d1a0b9a8dbd75ec466a4
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2017-02-22 11:40:16 +0100

    Add QuickReply

--- pymessenger2/__init__.py
+++ pymessenger2/__init__.py
@@ -11,3 +11,23 @@ class Element(ToJsonMixin):
     image_url = attr.ib(default=None)
     subtitle = attr.ib(default=None)
     buttons = attr.ib(default=None)
+
+
+@attr.s
+class QuickReply(ToJsonMixin):
+    """
+    See https://developers.facebook.com/docs/messenger-platform/send-api-reference/quick-replies
+
+    You may not give the payload and it'll be set to your title automatically.
+    """
+    content_type = attr.ib()
+    title = attr.ib(default=None)
+    payload = attr.ib(default=None)
+    image_url = attr.ib(default=None)
+
+    def __attrs_post_init__(self):
+        assert self.content_type in {'text', 'location'}
+        assert self.content_type == 'location' or self.title
+
+        if not self.payload:
+            self.payload = self.title
