05dfe07d60d5db6aa39a7a915433de865162bf51
mukesh <mkm96.ubt2014@iitr.ac.in>
2017-07-20 22:08:31 +0530

    python 3 compatibility

--- pymessenger/utils.py
+++ pymessenger/utils.py
@@ -18,11 +18,17 @@ def validate_hub_signature(app_secret, request_payload, hub_signature_header):
         pass
     else:
         digest_module = getattr(hashlib, hash_method)
-        hmac_object = hmac.new(str(app_secret), unicode(request_payload), digest_module)
+        if six.PY2:
+            hmac_object = hmac.new(
+                str(app_secret), unicode(request_payload), digest_module)
+        else:
+            hmac_object = hmac.new(
+                bytearray(app_secret), str(request_payload).encode('UTF-8'), digest_module)
         generated_hash = hmac_object.hexdigest()
         if hub_signature == generated_hash:
             return True
     return False
+
 
 
 def generate_appsecret_proof(access_token, app_secret):
@@ -37,6 +43,6 @@ def generate_appsecret_proof(access_token, app_secret):
     if six.PY2:
         hmac_object = hmac.new(str(app_secret), unicode(access_token), hashlib.sha256)
     else:
-        hmac_object = hmac.new(bytearray(app_secret, 'utf8'), str(access_token).encode('utf8'), hashlib.sha256)
+        hmac_object = hmac.new(bytearray(app_secret, 'UTF-8'), str(access_token).encode('UTF-8'), hashlib.sha256)
     generated_hash = hmac_object.hexdigest()
     return generated_hash
