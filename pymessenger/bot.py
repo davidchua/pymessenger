39ea6cace314ba8ad67918d7e47df58ad01939ca
Joel Benjamin <joelbenjamin093@gmail.com>
2017-08-13 19:44:18 +0530

    added easy generic button builder

--- pymessenger/bot.py
+++ pymessenger/bot.py
@@ -1,4 +1,5 @@
 import os
+import json
 from enum import Enum
 
 import requests
@@ -398,4 +399,73 @@ class Bot:
                         params=params,data=payload,
                         headers={'Content-type':'application/json'}
         )
+        
+        
+def build_generic_elements(self,elements):
+    """ arg format :({
+                        "element_data":[{"data":[title,img_url,sub_title,action_url]},]
+                        "button_data":[{"data":[url,title]}]
+                    })
+
+    """
+    element_list = []
+    button_list = []
+    len_element = len(elements['element_data'])
+    len_button = len(elements['button_data'])
+    
+    # constructs the button payload
+    for i in range(len_button):
+        button_list.append({
+            "type":"web_url",
+            "url":elements['button_data'][i]['data'][0],
+            "title":elements['button_data'][i]['data'][1],
+        })
+
+    # constructs the generic elements payload
+    for x in range(len_element):
+        element_list.append({
+            "title":elements['element_data'][x]['data'][0],
+            "image_url":elements['element_data'][x]['data'][1],
+            "subtitle":elements['element_data'][x]['data'][2],
+            "default_action":{
+                "type": "web_url",
+                "url": elements['element_data'][x]['data'][3],
+                "webview_height_ratio": "tall",
+            },
+            "buttons": button_list,
+        })
+    return element_list
+
+def generic_button_send(self,user_id,element_payload):
+    params = {
+        "access_token":self.access_token,
+
+    }
+
+    # builds the payload for elements in JSON format
+    elements = build_generic_elements(element_payload)
+    data=json.dumps({
+      "recipient":{
+        "id":user_id
+      },
+      "message":{
+        "attachment":{
+          "type":"template",
+          "payload":{
+            "template_type":"generic",
+            "elements": elements,
+          }
+        }
+      }
+    })
+
+    requests.post(
+        "https://graph.facebook.com/v2.6/me/messages",
+        params=params,
+        data=data,
+        headers={
+            'Content-type': 'application/json'
+        }
+    )
+
 
